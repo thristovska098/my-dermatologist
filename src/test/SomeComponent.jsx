4031b12b5af4a9e93a22b2655cd29ee92eb18d20
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-03-19 17:02:30 +0100

    * Configured eslint and prettier * Created dummy component for testing

---
+++ src/test/SomeComponent.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+/* eslint-disable */
+
+const SomeComponent = () => {
+  const blabla = 'jskaj';
+  const blablaaa = 'blabla';
+
+  return <div>Some component</div>;
+};
+
+export default SomeComponent;
