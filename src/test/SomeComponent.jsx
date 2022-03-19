07f649a2e44a230b2858a54159d2af5507d9169a
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-03-19 17:33:50 +0100

    * Added final form

--- src/test/SomeComponent.jsx
+++ src/test/SomeComponent.jsx
@@ -1,12 +1,21 @@
 // @flow
 import * as React from 'react';
+import { Form } from 'react-final-form';
 /* eslint-disable */
 
 const SomeComponent = (): React.Node => {
   const blabla = 'jskaj';
   const blablaaa = 'blabla';
 
-  return <div>Some component</div>;
+  return (
+    <Form onSubmit={() => console.log('Submitted')}>
+      {(props) => (
+        <form onSubmit={props.handleSubmit}>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </Form>
+  );
 };
 
 export default SomeComponent;
