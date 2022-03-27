319b56abb977d8eefe5c61e834f5f85425f2e451
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-03-27 19:21:28 +0200

    * Created Register Doctor page

--- src/ui/doctor-ui/RegisterDoctorPage.jsx
+++ src/ui/doctor-ui/RegisterDoctorPage.jsx
@@ -5,27 +5,39 @@ import * as React from 'react';
 import { Form } from 'react-final-form';
 import PersonalDataComponent from '../common/personal-data-component/PersonalDataComponent';
 import SubmitAndCancelFooter from '../common/submit-cancel-footer/SubmitAndCancelFooter';
+import { RowsContainer } from '../common/styles';
+import TextInputField from '../../components/final-form/TextInputField';
 
 // Constants
 import { FIELD_WIDTH_MAX } from '../common/constants';
-import { CANCEL_FIELD_LABEL, REGISTER_FIELD_LABEL } from './constants';
+import { CANCEL_FIELD_LABEL, REGISTER_FIELD_LABEL } from '../client-ui/constants';
+import { INVALID_DOCTOR_CODE_MESSAGE, MANDATORY_FIELD_MESSAGE } from '../common/messages';
+import { CODE_LABEL, LENGTH_OF_DOCTOR_CODE } from './constants';
 
 // Types
-import type { Patient } from '../../types/types.flow';
+import type { Doctor } from '../../types/types.flow';
 
 // Utils
 import { prepareDate } from '../common/utils';
+import { composeValidators, required, validateLength } from '../../components/validators';
 
-const RegisterClientPage = () => {
-  const handlingSubmit = (values: Patient) => {
+const RegisterDoctorPage = (): React.Node => {
+  const requiredValidator = required(MANDATORY_FIELD_MESSAGE);
+  const codeValidator = validateLength(INVALID_DOCTOR_CODE_MESSAGE, LENGTH_OF_DOCTOR_CODE);
+
+  const combinedCodeValidator = React.useCallback(
+    (fieldValue) => composeValidators([requiredValidator, codeValidator])(fieldValue),
+    [codeValidator, requiredValidator],
+  );
+  const handlingSubmit = (values: Object) => {
     // TODO: Implement this method when the BE is done.
     const preparedData = prepareData(values);
     console.log(preparedData);
   };
 
-  const prepareData = React.useCallback((values: Patient) => {
-    const patientData = values?.patient;
-    const { dateOfBirth, ...rest } = patientData;
+  const prepareData = React.useCallback((values: Doctor) => {
+    const doctorData = values?.doctor;
+    const { dateOfBirth, ...rest } = doctorData;
 
     if (dateOfBirth === undefined) {
       return { ...rest };
@@ -49,7 +61,15 @@ const RegisterClientPage = () => {
       subscription={{ values: true, form: true }}
       render={({ form, handleSubmit }) => (
         <>
-          <PersonalDataComponent fieldNamePrefix="patient" />
+          <RowsContainer>
+            <TextInputField
+              validate={combinedCodeValidator}
+              name="doctor.code"
+              label={CODE_LABEL}
+              width={FIELD_WIDTH_MAX}
+            />
+            <PersonalDataComponent fieldNamePrefix="doctor" />
+          </RowsContainer>
           <SubmitAndCancelFooter
             width={FIELD_WIDTH_MAX}
             handleSubmit={handleSubmit}
@@ -64,4 +84,4 @@ const RegisterClientPage = () => {
   );
 };
 
-export default RegisterClientPage;
+export default RegisterDoctorPage;
---
+++ src/ui/doctor-ui/constants.js
@@ -0,0 +1,2 @@
+export const CODE_LABEL = 'Code';
+export const LENGTH_OF_DOCTOR_CODE = 6;
