f6a1879ddef5e7f4d9ea8fcd7b23058c481e8d66
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-05-29 23:37:38 +0200

    * Created module for the BE

---
+++ my-dermatologist-be/src/main/java/com/md/mydermatologist/MyDermatologistApplication.java
@@ -0,0 +1,13 @@
+package com.md.mydermatologist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDermatologistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDermatologistApplication.class, args);
+	}
+
+}
