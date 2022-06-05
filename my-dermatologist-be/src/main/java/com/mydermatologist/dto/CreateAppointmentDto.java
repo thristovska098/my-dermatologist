2944c91c772a7800be74dea861d1bba4285bec39
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-06-05 18:41:03 +0200

    * Created dtos for creating appointment form

---
+++ my-dermatologist-be/src/main/java/com/mydermatologist/dto/CreateAppointmentDto.java
@@ -0,0 +1,27 @@
+package com.mydermatologist.dto;
+
+import com.mydermatologist.domain.Avatar;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * Dto that represents the model creating appointment by the patient.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAppointmentDto {
+
+   private String title;
+
+   private String description;
+
+   private DoctorDtoForCreatingAppointment doctor;
+
+   private List<Avatar> images;
+}
