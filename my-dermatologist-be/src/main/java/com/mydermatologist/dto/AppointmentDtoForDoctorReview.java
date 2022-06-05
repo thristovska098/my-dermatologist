43aa5dbdaef2a3a3755e92290da9cd29b73242b6
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-06-05 21:12:26 +0200

    * Implemented review appointments dtos for doctor

---
+++ my-dermatologist-be/src/main/java/com/mydermatologist/dto/AppointmentDtoForDoctorReview.java
@@ -0,0 +1,35 @@
+package com.mydermatologist.dto;
+
+import com.mydermatologist.domain.AppointmentStatus;
+import com.mydermatologist.domain.Avatar;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Dto that represents the model returned for doctor appointment results.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentDtoForDoctorReview {
+
+  private String id;
+
+  private String title;
+
+  private String description;
+
+  private AppointmentStatus appointmentStatus;
+
+  private Date createdOn;
+
+  private PatientDtoForDoctorReview patient;
+
+  private List<Avatar> images;
+}
---
+++ my-dermatologist-be/src/main/java/com/mydermatologist/dto/PatientDtoForDoctorReview.java
@@ -0,0 +1,31 @@
+package com.mydermatologist.dto;
+
+import com.mydermatologist.domain.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * Dto that represents the model returned for doctor appointment results.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientDtoForDoctorReview {
+
+  private String name;
+
+  private  String lastName;
+
+  private Gender gender;
+
+  private Date dateOfBirth;
+
+  private String email;
+
+  private String phone;
+}
