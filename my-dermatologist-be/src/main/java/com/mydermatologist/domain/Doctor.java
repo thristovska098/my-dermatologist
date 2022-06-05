45e9b5ddc5cc7b12072e50bb9a7599422b021409
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-06-05 17:53:25 +0200

    * Updated models for Doctor and Patient

--- my-dermatologist-be/src/main/java/com/mydermatologist/domain/Doctor.java
+++ my-dermatologist-be/src/main/java/com/mydermatologist/domain/Doctor.java
@@ -5,9 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.List;
 
 /**
  * Doctor model.
@@ -27,4 +31,7 @@ public class Doctor {
 
   @OneToOne
   private Office office;
+
+  @OneToMany(mappedBy = "doctor", cascade = {CascadeType.REFRESH, CascadeType.PERSIST,CascadeType.MERGE}, fetch = FetchType.LAZY)
+  private List<Appointment> appointments;
 }
--- my-dermatologist-be/src/main/java/com/mydermatologist/domain/Patient.java
+++ my-dermatologist-be/src/main/java/com/mydermatologist/domain/Patient.java
@@ -5,9 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.List;
 
 /**
  * Patient model.
@@ -24,4 +28,7 @@ public class Patient {
 
   @OneToOne
   private PersonalData personalData;
+
+  @OneToMany(mappedBy = "patient", cascade = {CascadeType.REFRESH, CascadeType.PERSIST,CascadeType.MERGE}, fetch = FetchType.LAZY)
+  private List<Appointment> appointments;
 }
