6884dc598523d476bcdc03148f3e1c1212e6c5cc
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-06-05 21:47:12 +0200

    * Created repos

---
+++ my-dermatologist-be/src/main/java/com/mydermatologist/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.mydermatologist.repository;
+
+import com.mydermatologist.domain.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository  extends JpaRepository<Patient, String> {
+}
