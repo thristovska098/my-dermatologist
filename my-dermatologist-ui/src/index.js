fefc9ffb0e5d9e86d127ff1bedb7865dd8b31cb5
Teodora Hristovska <Teodora.Hristovska@netcetera.com>
2022-06-15 23:27:40 +0200

    * Removed todo

--- my-dermatologist-ui/src/index.js
+++ my-dermatologist-ui/src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// TODO: Check if I can resolve the warning in Address component and add the <React.StrictMode> wrapper around the App.
 ReactDOM.render(<App />, document.getElementById('root'));
